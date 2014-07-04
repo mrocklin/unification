5e1e91df5b012085298b18a729b076e8b157d393
Matthew Rocklin <mrocklin@gmail.com>
2014-07-04 08:13:15 -0700

    add setup.py, version

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup
+import unification
+
+setup(name='unification',
+      version=unification.__version__,
+      description='Unification',
+      url='http://github.com/mrocklin/unification/',
+      author='https://raw.github.com/mrocklin/unification/master/AUTHORS.md',
+      maintainer='Matthew Rocklin',
+      maintainer_email='mrocklin@gmail.com',
+      license='BSD',
+      keywords='unification logic-programming dispatch',
+      packages=['unification']
+      long_description=(open('README.rst').read() if exists('README.rst')
+                        else ''),
+      zip_safe=False)
--- unification/__init__.py
+++ unification/__init__.py
@@ -1,3 +1,5 @@
 from .core import unify, reify
 from .more import unifiable
 from .variable import var, isvar, vars, variables
+
+__version__ = '0.2.0'
