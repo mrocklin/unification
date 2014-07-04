231cd5d5b3e6760500aa7b011e9276d469bd6d87
Matthew Rocklin <mrocklin@gmail.com>
2014-07-03 18:16:01 -0700

    match: register, also fix tests

--- unification/match.py
+++ unification/match.py
@@ -10,5 +10,12 @@ class Dispatcher(object):
 
     def __call__(self, *args, **kwargs):
         for k, v in self.funcs.items():
-            if unify(k, args):
+            if unify(k, args) != False:
                 return v(*args, **kwargs)
+        raise NotImplementedError("No match found")
+
+    def register(self, *signature):
+        def _(func):
+            self.add(signature, func)
+            return self
+        return _
--- unification/tests/test_match.py
+++ unification/tests/test_match.py
@@ -7,12 +7,25 @@ def dec(x):
     return x - 1
 
 def test_simple():
-
     d = Dispatcher('d')
 
     d.add((1,), inc)
     d.add((10,), dec)
 
-    d(1) == 2
-    d(10) == 9
+    assert d(1) == 2
+    assert d(10) == 9
 
+
+def test_register():
+    d = Dispatcher('d')
+
+    @d.register(1)
+    def f(x):
+        return 10
+
+    @d.register(2)
+    def f(x):
+        return 20
+
+    assert d(1) == 10
+    assert d(2) == 20
