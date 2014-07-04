a3671b96b4b8ae1b71097abdf0b8877897adc263
Matthew Rocklin <mrocklin@gmail.com>
2014-07-03 18:19:03 -0700

    match: assert that dispatchers properly raise NotImplementedError

--- unification/tests/test_match.py
+++ unification/tests/test_match.py
@@ -1,10 +1,13 @@
 from unification.match import *
+from unification.utils import raises
 
 def inc(x):
     return x + 1
 
+
 def dec(x):
     return x - 1
+
 
 def test_simple():
     d = Dispatcher('d')
@@ -14,6 +17,13 @@ def test_simple():
 
     assert d(1) == 2
     assert d(10) == 9
+
+
+def test_raises_error():
+    d = Dispatcher('d')
+
+    assert raises(NotImplementedError, lambda : d(1, 2, 3))
+
 
 
 def test_register():
